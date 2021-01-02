5bfae31b18f2600bc5e24942babe6e960f2e6a10
Flavio <33604967+FLAFLALEBG@users.noreply.github.com>
2021-01-02 22:26:37 +0100

    Add some function

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,8 +1,9 @@
 import os
 import sys, traceback
+import pwnedpasswords
 
 # Variables :
-profile = []
+profil = []
 
 
 def main():
@@ -19,7 +20,8 @@ ________________________________________________________________________________
 8 8888           .8'   `8. `88888.   8b   `8.`8888. 8b   `8.`8888.        `8.`888`8.`88'        `8 8888       ,8P  8 8888 `8b.     8 8888        ,88'   |/\|                  
 8 8888          .888888888. `88888.  `8b.  ;8.`8888 `8b.  ;8.`8888         `8.`8' `8,`'          ` 8888     ,88'   8 8888   `8b.   8 8888    ,o88P'     |/\|                  
 8 8888         .8'       `8. `88888.  `Y8888P ,88P'  `Y8888P ,88P'          `8.`   `8'              `8888888P'     8 8888     `88. 8 888888888P'        |/\|                  \033[32m	
-\033[31m                                                                                                                                                        |/\|	
+\033[31m
+                                                                                                                                                        |/\|	
      ,o888888o.             .8.          8888888 8888888888 8 8888        8 8 8888888888   8 888888888o.    8 8888 b.             8      ,o888888o.     |/\|                  
     8888     `88.          .888.               8 8888       8 8888        8 8 8888         8 8888    `88.   8 8888 888o.          8     8888     `88.   |/\|                  
  ,8 8888       `8.        :88888.              8 8888       8 8888        8 8 8888         8 8888     `88   8 8888 Y88888o.       8  ,8 8888       `8.  |/\|                  
@@ -32,19 +34,55 @@ ________________________________________________________________________________
      `8888888P'    .8'       `8. `88888.       8 8888       8 8888        8 8 888888888888 8 8888     `88.  8 8888 8            `Yo      `8888888P'     |/\|                  \033[31m
 ________________________________________________________________________________________________________________________________________________________|/\|
 ''')
+    menu()
+
+
+def menu():
     print("1) Créer un profile")
     print("2) Choisir un profile")
     print("3) Générer un mot de passe")
+    print("4) Verifier un mot de passe")
     main_navigation = int(input("> "))
 
     if main_navigation == 1:
         create_profil()
 
+    if main_navigation == 2:
+        choose_profil()
+
 
 def create_profil():
     # Création variable "profile" et demande le mot de passe
-    new_profile = input("Nouveau profile > ")
-    profile_len = len(profile)
+    new_profil = input("Nouveau profile : ")
+    profil.append(new_profil)
+    profile_len = len(profil)
+    print("Parfait, profile crée. Retour au menu.")
+    menu()
+
+
+def choose_profil():
+    print("Vous avez : ", len(profil), "profile(s)")
+    if len(profil) >= 5:
+        delete_profil()
+
+    print("Voici vos profils : ")
+    print("1)", profil[0])
+    print("2)", profil[1])
+    print("3)", profil[2])
+    print("4)", profil[3])
+    print("5)", profil[4]) 
+
+
+def delete_profil():
+    print("Vous avez plus de 5 profils veuillez en supprimer un.")
+    print("Voici vos profils : ")
+    print("1)", profil[0])
+    print("2)", profil[1])
+    print("3)", profil[2])
+    print("4)", profil[3])
+    print("5)", profil[4])
+    del_profil_number = int(input("> "))
+    del profil[del_profil_number]
 
 
 if __name__ == "__main__":
